9089e3aeec2055ac14a7822b1dd6a63ccb90f14b
Mokau007 <paballomasemola@gmail.com>
2023-12-06 17:11:31 +0200

    Admin portal config

---
+++ src/PhaseFmPortal.Web/src/app/admin-portal-authentication/admin-portal-authentication.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminPortalAuthenticationRoutingModule } from './admin-portal-authentication-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdminPortalAuthenticationRoutingModule
+  ]
+})
+export class AdminPortalAuthenticationModule { }
